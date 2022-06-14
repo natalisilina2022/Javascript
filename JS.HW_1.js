a4a91099b52d67ac3e8833495782bca53727e4df
Наталья Силина <natalisilina@gmail.com>
2022-06-14 11:28:21 +0300

    add js homework file

---
+++ JS.HW_1.js
@@ -0,0 +1,45 @@
+var item_1;
+item_1 = 5;
+console.log(item_1);
+var item_2;
+item_2 = 3;
+console.log(item_2);
+var item_3;
+item_3 = item_1 + item_2;
+console.log(item_3);
+var item_4;
+item_4 = "Yolochka";
+console.log(item_4);
+console.log(item_3 + item_4);
+console.log(item_3 * item_4);
+var item_5;
+item_5 = item_3;
+var item_6;
+var item_6_type;
+item_6 = 15;
+// присвоить переменной item_6_type тип переменной item_6
+item_6_type = item_6;
+console.log("-- item_6 == " + item_6 + " item_6_type == " + item_6_type + " --");
+var item_7; 
+item_7 = "item_6"; 
+var item_7_type;
+item_7_type = item_7; 
+console.log("-- item_7 == " + item_7 + " item_7_type == " + item_7_type + " --");
+var age_1 = 10; 
+var age_2 = 18;
+var age_3 = 60; 
+if (age_1 < age_2) {
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then" );
+} else {
+    console.log( "Technical work");
+}
+if ((age_1 >= age_2) && (age_1 < age_3)) {
+    console.log("Welcome!"); 
+} else {
+    console.log( "Technical work");
+}
+if (age_1 > age_3) {
+    console.log( "Keep calm and look Culture channel" );
+} else {
+    console.log( "Technical work");
+}
