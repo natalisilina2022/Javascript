d76901fb6eaf365e3511be7cc58920dc065f8acb
Наталья Силина <natalisilina@gmail.com>
2022-06-20 14:19:17 +0300

    redacted

--- JS.HW_1.js
+++ JS.HW_1.js
@@ -1,44 +1,38 @@
-var item_1;
+let item_1;
 item_1 = 5;
 console.log(item_1);
-var item_2;
+let item_2;
 item_2 = 3;
 console.log(item_2);
-var item_3;
+let item_3;
 item_3 = item_1 + item_2;
 console.log(item_3);
-var item_4;
+let item_4;
 item_4 = "Yolochka";
 console.log(item_4);
 console.log(item_3 + item_4);
 console.log(item_3 * item_4);
-var item_5;
+let item_5;
 item_5 = item_3;
-var item_6;
-var item_6_type;
+let item_6;
+let item_6_type;
 item_6 = 15;
 // присвоить переменной item_6_type тип переменной item_6
-item_6_type = item_6;
+item_6_type = typeof item_6;
 console.log("-- item_6 == " + item_6 + " item_6_type == " + item_6_type + " --");
-var item_7; 
+let item_7; 
 item_7 = "item_6"; 
-var item_7_type;
-item_7_type = item_7; 
+let item_7_type;
+item_7_type = typeof item_7; 
 console.log("-- item_7 == " + item_7 + " item_7_type == " + item_7_type + " --");
-var age_1 = 10; 
-var age_2 = 18;
-var age_3 = 60; 
+let age_1 = 10; 
+let age_2 = 18;
+let age_3 = 60; 
 if (age_1 < age_2) {
-    console.log("You don’t have access cause your age is " + age_1 + " It’s less then" );
-} else {
-    console.log( "Technical work");
-}
-if ((age_1 >= age_2) && (age_1 < age_3)) {
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then 18" );
+} else if ((age_1 >= age_2) && (age_1 < age_3)) {
     console.log("Welcome!"); 
-} else {
-    console.log( "Technical work");
-}
-if (age_1 > age_3) {
+} else if (age_1 > age_3) {
     console.log( "Keep calm and look Culture channel" );
 } else {
     console.log( "Technical work");
