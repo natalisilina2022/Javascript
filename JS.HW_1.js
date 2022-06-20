08f3d76bdb0444f1d0f5b166f83592758211fc34
Наталья Силина <natalisilina@gmail.com>
2022-06-20 16:37:45 +0300

    redacted 2

--- JS.HW_1.js
+++ JS.HW_1.js
@@ -21,7 +21,7 @@ item_6 = 15;
 item_6_type = typeof item_6;
 console.log("-- item_6 == " + item_6 + " item_6_type == " + item_6_type + " --");
 let item_7; 
-item_7 = "item_6"; 
+item_7 = String(item_6); 
 let item_7_type;
 item_7_type = typeof item_7; 
 console.log("-- item_7 == " + item_7 + " item_7_type == " + item_7_type + " --");
